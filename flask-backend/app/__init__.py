5c7a1c1cdcc2940e879a524b600b267c9fd26282
Lawrence Fung <4619010@student.isf.edu.hk>
2025-08-08 12:17:52 +0800

    updated vue frontend, resolved bugs, the server can run now

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.config import Config
 from app.models import db
 from datetime import timedelta, datetime, timezone
@@ -10,12 +10,21 @@ def create_app():
 
     db.init_app(app)
 
-    from app.routes import main
-    app.register_blueprint(main)
+    from app.routes import auth, users, articles, quizzes, translations, word_bank
+    app.register_blueprint(auth)
+    app.register_blueprint(users)
+    app.register_blueprint(articles)
+    app.register_blueprint(quizzes)
+    app.register_blueprint(translations)
+    app.register_blueprint(word_bank)
 
     app.config["JWT_SECRET_KEY"] = "your_jwt_secret"
     app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=14)
     jwt = JWTManager(app)
+    
+    @app.route("/", methods=['GET'])
+    def index():
+        return jsonify({"message": "API is running!"}), 200
 
     return app
